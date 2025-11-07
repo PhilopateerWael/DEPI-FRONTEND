ba4fdc98e741072b20b93fa8e9f39769ef370172
philo wael <reconstructed.philo@gmail.com>
2025-11-07 02:17:08 +0200

    oopsie #3

--- src/Api.ts
+++ src/Api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
+import env from "./env";
 
 export const api = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: env.BACKEND_URL,
     withCredentials : true
 });
 
